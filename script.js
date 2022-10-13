ff2e3021efe8456e05baaabf7ed29785819b0cd4
Ashley Chatman <ashchat2008@gmail.com>
2022-10-13 14:29:43 -0400

    -Created array

---
+++ script.js
@@ -0,0 +1,13 @@
+/*Randomly gets computer choice of Rock, Paper, and Scissor*/
+const getComputerChoice = () =>{
+    
+    const computerOptions =['Rock','Paper','Scissors']; //An array that gives three choices of Rock,Paper, and Scissors
+
+    const computerSelection = computerOptions[Math.floor(Math.random()*computerOptions.length)]; //Cycles through the array to get random computer choices
+
+console.log (computerSelection);
+  
+};
+getComputerChoice();
+
+
